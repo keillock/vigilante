f63c99bb7b67160770478a88b9c28e186fede810
keillock <keillockwood@gmail.com>
2024-11-24 00:20:15 -0600

    Fix Home page

---
+++ app/home/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <div className="flex flex-col items-center justify-center h-screen bg-gray-50">
+        <h1 className="text-3xl font-bold">Bienvenido a Vigilante</h1>
+        <p className="text-gray-500 mt-4">Esta es la homepage.</p>
+      </div>
+    );
+  }
