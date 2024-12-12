29855821bf17f27c87db3078258e1964eccc97a4
keillock <keillockwood@gmail.com>
2024-12-12 01:17:25 -0600

    iOS PWA functionality lol

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,6 +52,10 @@ export default function RootLayout({
       <head>
         <link rel="manifest" href="/manifest.json" />
         <title>{metadata.title}</title>
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+  <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+  <meta name="apple-mobile-web-app-title" content="Vigilante" />
+  <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <meta name="description" content={metadata.description} />
         <meta property="og:title" content={metadata.openGraph.title} />
         <meta property="og:description" content={metadata.openGraph.description} />
